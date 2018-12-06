2b1eef4918917cc28eb0074c02623e38ccfd7e37
thediscomole <kim.roder@gmail.com>
2018-12-06 19:10:39 +0000

    making the layer more general purpose and straight forward

--- pytorch/hierarchyreduction.py
+++ pytorch/hierarchyreduction.py
@@ -1,34 +1,33 @@
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class HierarchyReduction(nn.Module):
+class HierarchyEncoder (nn.Module):
     """
-    NN layer that reduces the information hierarchy of the input.
+    NN layer that reduces the information hierarchy of the input. By slicing the
+    inputs into n batches, and applying a cell that reduces a batch into a
+    single lower-dimensional output, you can reduce the informational hierarchy
+    of your data "indefinitely".
 
-    By slicing the inputs into n batches, and applying a cell that reduces a batch
-    into a single lower dimensional output, you can reduce the inforamtional hierarchy
-    of your data indefinitely without running into Tensor/ugly-padding limits.
+    args:
+    - cell: takes an input with shape [1,sequence,<dimensions>] and returns an
+            output with shape [1,<dimensions>]
 
-    Args:
-        cell: takes an input with shape [old_batches,...] and returns an output with
-              shape [...]
-
-    source:
+    source: https://github.com/TheDiscoMole/HierarchyReduction
     """
-    def __init__ (self, cell):
-        super(HierarchyReduction, self).__init__()
+    def __init__ (self,cell):
+        super(HierarchyReduction,self).__init__()
         self.cell = cell
 
     """
     inputs:
-        slices: example input [0,9,15, ... len(inputs)]
-        inputs: [stached_batches,...]
+    - slices: [slice,index]
+    - inputs: [1,sequence,<dimensions>]
     outputs:
-        output: [un_stacked_baches,...]
+    - output: [1,sequence,<dimensions>]
     """
-    def __call__ (self, slices, inputs):
-        outputs = [
-            cell( inputs.narrow(0,slices[i],slices[i+1]) )
-            for i in range(slices.size()-1)]
-        return torch.stack(outputs)
+    def forward (self,slices,inputs):
+        return torch.stack([
+            cell( inputs.narrow(0,slices[i][0],slices[i][1]) )
+            for i in range(slices.size()-1)])
